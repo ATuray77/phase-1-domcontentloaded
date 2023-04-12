4445bbc6f75a91cce0d713c64f98c623655f1298
Aruna Turay <tura_arun@yahoo.com>
2023-04-12 12:45:03 -0400

    content

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+  let myText = document.getElementById("text")
+  myText.textContent = "This is really cool!"
+  
+})
+
